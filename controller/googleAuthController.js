43945a5e5be0b057c37a27803cfa05be35bc23e6
Pratishrutintspl <pratishrutintspl@gmail.com>
2026-08-27 17:09:00 +0530

    commit by pratishruti on 27-08 at 05.08

--- controller/googleAuthController.js
+++ controller/googleAuthController.js
@@ -1,60 +1,46 @@
-const googleAuthService =
-require("../services/googleAuthService");
+const googleAuthService = require("../services/googleAuthService");
 
+const Response = require("../utils/response");
 
-const Response =
-require("../utils/response");
-
-const message =
-require("../constants/message");
+const message = require("../constants/message");
 
 const googleCallback = async (req, res) => {
-
   try {
-
     const googleUser = {
       name: req.user.name,
       email: req.user.email,
       googleId: req.user.googleId,
-      profileImage: req.user.profileImage
+      profileImage: req.user.profileImage,
     };
 
-
-    const user =
-      await googleAuthService.googleLoginUser(googleUser);
-
+    const user = await googleAuthService.googleLoginUser(googleUser);
 
     const userData = {
       id: user._id,
       name: user.name,
       email: user.email,
-      profileImage: user.profileImage
+      profileImage: user.profileImage,
     };
 
-
-    const encodedUser = Buffer
-      .from(JSON.stringify(userData))
-      .toString("base64");
-
-
-    res.redirect(
-      `${process.env.FRONTEND_URL}/google-success?token=${user.accessToken}&user=${encodedUser}`
+    const encodedUser = Buffer.from(JSON.stringify(userData)).toString(
+      "base64",
     );
-
-
-  } catch(error){
-
-    res.status(500).json({
-      message:error.message
+    console.log("GOOGLE REDIRECT DATA:", {
+      token: user.accessToken,
+      userData,
+      encodedUser,
     });
 
+    res.redirect(
+      `${process.env.FRONTEND_URL}/google-success?token=${user.accessToken}&user=${encodedUser}`,
+    );
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
   }
-
 };
 
-
-
-
 module.exports = {
-  googleCallback
+  googleCallback,
 };
