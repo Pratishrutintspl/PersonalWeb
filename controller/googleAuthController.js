e92186c866a2adfa94e32027d283fb5509b66a2f
Pratishrutintspl <pratishrutintspl@gmail.com>
2026-08-27 16:18:19 +0530

    commit by pratishruti on 27-08 at 04.18

--- controller/googleAuthController.js
+++ controller/googleAuthController.js
@@ -9,51 +9,47 @@ const message =
 require("../constants/message");
 
 const googleCallback = async (req, res) => {
-  console.log("ddddddd")
+
   try {
 
     const googleUser = {
-
       name: req.user.name,
-
       email: req.user.email,
-
       googleId: req.user.googleId,
-
       profileImage: req.user.profileImage
-
     };
 
 
     const user =
-      await googleAuthService.googleLoginUser(
-        googleUser
-      );
+      await googleAuthService.googleLoginUser(googleUser);
 
 
-    console.log(
-      "Google Login Success:",
-      user.email
-    );
+    const userData = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImage: user.profileImage
+    };
+
+
+    const encodedUser = Buffer
+      .from(JSON.stringify(userData))
+      .toString("base64");
 
 
     res.redirect(
-      `${process.env.FRONTEND_URL}/google-success?token=${user.accessToken}`
+      `${process.env.FRONTEND_URL}/google-success?token=${user.accessToken}&user=${encodedUser}`
     );
 
 
-  } catch (error) {
-
-    console.log(
-      "Google Callback Error:",
-      error
-    );
+  } catch(error){
 
     res.status(500).json({
       message:error.message
     });
 
   }
+
 };
 
 
